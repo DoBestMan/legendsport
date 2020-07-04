82fcc7cbf0f3e5c9c794c4302e00b306122f3dab
Carnage <t.carnage@gmail.com>
2020-07-04 15:18:12 +0100

    Added websocket and scheduler config

--- infrastructure/build/deploy-staging.sh
+++ infrastructure/build/deploy-staging.sh
@@ -12,6 +12,8 @@ cp infrastructure/kubernetes/staging/*.yaml /tmp/staging
 echo "Deploying: $BACKEND_IMAGE and $FRONTEND_IMAGE"
 
 sed "s!BACKEND_IMAGE!${BACKEND_IMAGE}!g" ./infrastructure/kubernetes/staging/templated/php-fpm.yaml | sed "s!FRONTEND_IMAGE!${FRONTEND_IMAGE}!g" > /tmp/staging/php-fpm.yaml
+sed "s!BACKEND_IMAGE!${BACKEND_IMAGE}!g" ./infrastructure/kubernetes/staging/templated/scheduler.yaml > /tmp/staging/scheduler.yaml
+sed "s!BACKEND_IMAGE!${BACKEND_IMAGE}!g" ./infrastructure/kubernetes/staging/templated/websockets.yaml > /tmp/staging/websockets.yaml
 
 gcloud container clusters get-credentials --region "$CLOUDSDK_COMPUTE_REGION" "$CLOUDSDK_CONTAINER_CLUSTER"
 
