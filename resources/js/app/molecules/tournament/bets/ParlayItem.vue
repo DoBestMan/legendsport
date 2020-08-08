c34c37a32adf0245743196c042b521bdbe097bfb
audricus19 <audricus2019@gmail.com>
2020-08-07 18:49:14 -0700

    changed tournament/bets/parlay module styles

--- resources/js/app/molecules/tournament/bets/ParlayItem.vue
+++ resources/js/app/molecules/tournament/bets/ParlayItem.vue
@@ -1,22 +1,34 @@
 <template>
-    <div class="event-frm">
-        <div class="data-frm">
-            <div class="trash" @click="remove">
-                <i class="icon fas fa-trash-alt"></i>
+    <div class="bet">
+        <div class="bet__details">
+            <div class="bet__details__icon" @click="remove">
+                <i class="icon icon--sport-nfl icon--micro"></i>
             </div>
 
-            <BetContent
-                :scoreAway="game.scoreAway"
-                :scoreHome="game.scoreHome"
-                :startsAt="game.startsAt"
-                :teamHome="game.teamHome"
-                :teamAway="game.teamAway"
-                :selectedTeam="team"
-                :odd="oddValue"
-                :type="pendingOdd.type"
-                :type-extra="oddExtra"
-            />
+            <div class="bet__details__content">
+                <div class="bet__details__content__title">
+                    {{ game.teamHome }} - {{ game.teamAway }}
+                </div>
+                <div class="bet__details__content__subtitle">{{ game.startsAt | toDateTime }}</div>
+            </div>
+
+            <div class="bet__details__icon">
+                <i class="icon icon--delete icon--micro"></i>
+            </div>
         </div>
+
+        <BetContent
+            :scoreAway="game.scoreAway"
+            :scoreHome="game.scoreHome"
+            :startsAt="game.startsAt"
+            :teamHome="game.teamHome"
+            :teamAway="game.teamAway"
+            :selectedTeam="team"
+            :odd="oddValue"
+            :type="pendingOdd.type"
+            :type-extra="oddExtra"
+            status="Win"
+        />
     </div>
 </template>
 
@@ -25,7 +37,7 @@ import Vue, { PropType } from "vue";
 import { DeepReadonly } from "../../../../general/types/types";
 import { Game } from "../../../types/game";
 import { PendingOdd } from "../../../types/window";
-import {getOddExtra, getPendingOddTeam, getPendingOddValue} from "../../../utils/game/bet";
+import { getOddExtra, getPendingOddTeam, getPendingOddValue } from "../../../utils/game/bet";
 import BetContent from "./BetContent.vue";
 import { Odd } from "../../../types/odd";
 
@@ -53,7 +65,7 @@ export default Vue.extend({
 
         oddExtra(): string {
             return this.odd ? getOddExtra(this.pendingOdd, this.odd) : "";
-        }
+        },
     },
 
     methods: {
