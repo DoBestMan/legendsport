9cc431fa64a69e9c87b187c7928a19738bff3ca7
audricus19 <audricus2019@gmail.com>
2020-08-10 10:55:39 -0700

    added slider section component

---
+++ resources/js/app/molecules/general/SliderSection.vue
@@ -0,0 +1,71 @@
+<template>
+    <section>
+        <div class="layout__content__container__slider">
+            <div class="sliders">
+                <div class="sliders--desktop">
+                    <div class="slider" style="background-image: url(assets/i/slider-1@3x.jpg)">
+                        <div class="slider__paginations">
+                            <div class="pagination"></div>
+                            <div class="pagination pagination--active"></div>
+                            <div class="pagination"></div>
+                            <div class="pagination"></div>
+                            <div class="pagination"></div>
+                        </div>
+                        <div class="slider__content">
+                            <div class="slider__content__title">World Series Win Bonus</div>
+                            <div class="slider__content__description">Earn Up To $50 Bonus!</div>
+                        </div>
+                        <div class="slider__controls">
+                            <div class="slider__controls__control">
+                                <i class="icon icon--arrow-left icon--color--dark-3"></i>
+                            </div>
+                            <div class="slider__controls__control">
+                                <i class="icon icon--arrow-right icon--color--dark-3"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="layout__content__container__slider--mobile">
+            <div class="sliders">
+                <div class="sliders--mobile">
+                    <div class="slider" style="background-image: url(assets/i/slider-1@3x.jpg)">
+                        <div class="slider__content">
+                            <div class="slider__content__title">World Series Win Bonus</div>
+                            <div class="slider__content__description">Earn Up To $50 Bonus!</div>
+                        </div>
+                    </div>
+                    <div class="slider" style="background-image: url(assets/i/slider-1@3x.jpg)">
+                        <div class="slider__content">
+                            <div class="slider__content__title">World Series Win Bonus</div>
+                            <div class="slider__content__description">Earn Up To $50 Bonus!</div>
+                        </div>
+                    </div>
+                    <div class="slider" style="background-image: url(assets/i/slider-1@3x.jpg)">
+                        <div class="slider__content">
+                            <div class="slider__content__title">World Series Win Bonus</div>
+                            <div class="slider__content__description">Earn Up To $50 Bonus!</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="paginations d--only--mobile">
+                <div class="pagination"></div>
+                <div class="pagination pagination--active"></div>
+                <div class="pagination"></div>
+                <div class="pagination"></div>
+                <div class="pagination"></div>
+            </div>
+        </div>
+    </section>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+    name: "SliderSection",
+});
+</script>
