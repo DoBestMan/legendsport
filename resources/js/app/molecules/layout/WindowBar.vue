dc5af03a5b26da2b62db950b07e4d2f848143e7c
audricus19 <audricus2019@gmail.com>
2020-08-10 11:03:36 -0700

    fixed select window tab issue

--- resources/js/app/molecules/layout/WindowBar.vue
+++ resources/js/app/molecules/layout/WindowBar.vue
@@ -1,34 +1,55 @@
 <template>
     <section class="layout__header">
-        <!-- <HorizontallyScrollable class="col tabs-row-frm">
-            <div class="tabs-frm">
-                <div class="tab-frm">
-                    <router-link tag="button" class="btn tab" to="/" exact>
-                        <i class="icon fas fa-home"></i>
-                        Home
-                    </router-link>
-                </div>
-
-                <div class="tab-frm" v-for="window in windows" :key="window.id">
-                    <router-link tag="button" class="btn tab" :to="`/tournaments/${window.id}`">
-                        {{ window.tournament.name }}
-                    </router-link>
-                    <div class="delete" style="margin-left: -5px" @click="closeWindow(window)" />
-                </div>
-            </div>
-        </HorizontallyScrollable> -->
-        <div>
-            <router-link class="tab" to="/" exact>
+        <!-- <div>
+            <router-link
+                class="tab"
+                to="/"
+                :class="{
+                    'tab--active': isHomeSelected(),
+                }"
+                @click="selectWindowTabs(-1)"
+            >
                 <i class="icon icon--home icon--micro m--r--2"></i>
                 Home
             </router-link>
         </div>
 
         <div v-for="window in windows" :key="window.id">
-            <router-link class="tab" :to="`/tournaments/${window.id}`">
+            <router-link
+                class="tab"
+                :to="`/tournaments/${window.id}`"
+                :class="{
+                    'tab--active': isWindowsSelected(window.id),
+                }"
+                @click="selectWindowTabs(window.id)"
+            >
                 <i class="icon icon--cup icon--micro m--r--2"></i>
                 {{ window.tournament.name }}
             </router-link>
+        </div> -->
+
+        <div
+            class="tab"
+            :class="{
+                'tab--active': isHomeSelected(),
+            }"
+            @click="selectWindowTabs(-1)"
+        >
+            <i class="icon icon--home icon--micro m--r--2"></i>
+            Home
+        </div>
+
+        <div
+            v-for="window in windows"
+            :key="window.id"
+            class="tab"
+            :class="{
+                'tab--active': isWindowsSelected(window.id),
+            }"
+            @click="selectWindowTabs(window.id)"
+        >
+            <i class="icon icon--cup icon--micro m--r--2"></i>
+            {{ window.tournament.name }}
         </div>
     </section>
 </template>
@@ -42,6 +63,12 @@ export default Vue.extend({
     name: "WindowBar",
     components: { HorizontallyScrollable },
 
+    data() {
+        return {
+            windowId: -1,
+        };
+    },
+
     computed: {
         windows(): Window[] {
             return Object.values(this.$stock.getters["window/windows"]);
@@ -51,6 +78,23 @@ export default Vue.extend({
     methods: {
         closeWindow(window: Window): void {
             this.$stock.commit("window/closeWindow", window.id);
+        },
+
+        selectWindowTabs(window_id: number) {
+            this.windowId = window_id;
+            if (window_id === -1) {
+                this.$router.push("/");
+            } else {
+                this.$router.push(`/tournaments/${window_id}`);
+            }
+        },
+
+        isHomeSelected(): boolean {
+            return this.windowId === -1;
+        },
+
+        isWindowsSelected(window_id: number): boolean {
+            return window_id === this.windowId;
         },
     },
 });
