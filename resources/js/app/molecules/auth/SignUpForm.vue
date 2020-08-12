2bb9c879b3a9609c56488521c971f84940fddb6f
audricus19 <audricus2019@gmail.com>
2020-08-11 20:26:31 -0700

    changed sign up form style

--- resources/js/app/molecules/auth/SignUpForm.vue
+++ resources/js/app/molecules/auth/SignUpForm.vue
@@ -1,5 +1,5 @@
 <template>
-    <form @submit.prevent="signUp">
+    <!-- <form @submit.prevent="signUp">
         <div class="form-group">
             <label for="form-name">Name</label>
             <FormInput
@@ -53,7 +53,90 @@
         <div class="form-group mt-5">
             <button type="submit" class="btn btn-action btn-block btn-lg">Sign Up</button>
         </div>
-    </form>
+    </form> -->
+
+    <div class="layout">
+        <div class="layout__navbar layout__navbar--transparent">
+            <div class="logo">
+                <div class="logo__icon">LS</div>
+                <div class="logo__text d--only--desktop">LEGEND SPORTS</div>
+            </div>
+        </div>
+        <div class="layout__center">
+            <div class="layout__center__wrapper">
+                <h2 class="subtitle subtitle--light text--center">JOIN OUR COMMUNITY</h2>
+                <form class="form" @submit.prevent="signUp">
+                    <div class="form__control m--b--4">
+                        <label class="label label--large">E-MAIL</label>
+                        <FormInput
+                            id="form-name"
+                            inputClass="input input--large"
+                            placeHolder="Ex. john@google.com"
+                            type="text"
+                            autocomplete="username"
+                            :errors="errors.name"
+                            v-model="name"
+                            required
+                        />
+                    </div>
+                    <div class="form__control m--b--4">
+                        <label class="label label--large">Email</label>
+                        <FormInput
+                            id="form-email"
+                            inputClass="input input--large"
+                            placeHolder="Pick a email"
+                            type="email"
+                            autocomplete="email"
+                            :errors="errors.email"
+                            v-model="email"
+                            required
+                        />
+                    </div>
+                    <div class="form__control m--b--4">
+                        <label class="label label--large">PASSWORD</label>
+                        <FormInput
+                            id="form-password"
+                            inputClass="input input--large"
+                            placeHolder="Minimum 8 characters"
+                            type="password"
+                            autocomplete="current-password"
+                            :errors="errors.password"
+                            v-model="password"
+                            minlength="8"
+                            required
+                        />
+                    </div>
+                    <div class="form__control m--b--10">
+                        <label class="label label--large">CONFIRM PASSWORD</label>
+                        <FormInput
+                            id="form-password-confirmation"
+                            inputClass="input input--large"
+                            placeHolder="Minimum 8 characters"
+                            type="password"
+                            autocomplete="current-password"
+                            :errors="errors.password_confirmation"
+                            v-model="passwordConfirmation"
+                            minlength="8"
+                            required
+                        />
+                    </div>
+                    <button type="submit" class="button button--large m--b--6">JOIN</button>
+                </form>
+                <div class="paragraph paragraph--small">
+                    Already have an account?
+                    <a href="/login" class="link">Log in</a>
+                </div>
+                <div class="seperator"></div>
+                <div class="paragraph paragraph--tiny">
+                    By creating an account you are agreeing to Legend Sports’s
+                    <a href="#" class="link">Terms of Use</a>
+                    and
+                    <a href="#" class="link">Privacy Policy</a>
+                    and to be updated about Legend Sports products, news, and promotions.
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script lang="ts">
