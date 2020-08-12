962d6bf8348dce429eec1fb9953d9a693f31f4b4
audricus19 <audricus2019@gmail.com>
2020-08-11 20:33:09 -0700

    added some routes in routing index

--- resources/js/app/routing/index.ts
+++ resources/js/app/routing/index.ts
@@ -4,13 +4,23 @@ import NotFoundView from "../views/NotFoundView.vue";
 import TournamentView from "../views/TournamentView.vue";
 import { scrollBehavior } from "./utils";
 import HistoryView from "../views/HistoryView.vue";
+import SignInForm from "../molecules/auth/SignInForm.vue";
+import SignUpForm from "../molecules/auth/SignUpForm.vue";
+import AboutView from "../views/AboutView.vue";
+import SoonView from "../views/SoonView.vue";
+import SupportView from "../views/SupportView.vue";
 
 export const createRouter = (): VueRouter => {
     const routes: RouteConfig[] = [
         { path: "/", component: HomeView },
+        { path: "/login", component: SignInForm },
+        { path: "/signup", component: SignUpForm },
         { path: "/tournaments/:tournamentId", component: TournamentView },
-        { path: "/history", component: HistoryView},
-        { path: "*", component: NotFoundView }
+        { path: "/history", component: HistoryView },
+        { path: "/about", component: AboutView },
+        { path: "/soon", component: SoonView },
+        { path: "/support", component: SupportView },
+        { path: "*", component: NotFoundView },
     ];
 
     return new VueRouter({
