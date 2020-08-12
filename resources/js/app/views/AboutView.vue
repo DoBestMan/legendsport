0c6007d808e24bb780a9226e5960ffd4d81d29c0
audricus19 <audricus2019@gmail.com>
2020-08-11 20:30:31 -0700

    added AboutView component

---
+++ resources/js/app/views/AboutView.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="layout">
+        <div class="layout__content">
+            <div class="layout__content__about">
+                <h1 class="title">Title Here</h1>
+                <h2 class="subtitle">SUBTITLE</h2>
+                <p class="paragraph">
+                    So strongly and metaphysically did I conceive of my situation then, that while
+                    earnestly watching his motions, I seemed distinctly to perceive that my own
+                    individuality was now merged in a joint stock company of two; that my free will
+                    had received a mortal wound; and that another's mistake or misfortune might
+                    plunge innocent me into unmerited disaster and death.
+                </p>
+                <p class="paragraph">
+                    Therefore, I saw that here was a sort of interregnum in Providence; for its
+                    even-handed equity never could have so gross an injustice. And yet still further
+                    pondering—while I jerked him now and then from between the whale and ship, which
+                    would threaten to jam him—still further pondering, I say, I saw that this
+                    situation of mine was the precise situation of every mortal that breathes; only,
+                    in most cases, he, one way or other, has this Siamese connexion with a plurality
+                    of other mortals. If your banker breaks, you snap; if your apothecary by mistake
+                    sends you poison in your pills, you die.
+                </p>
+                <p class="paragraph">
+                    True, you may say that, by exceeding caution, you may possibly escape these and
+                    the multitudinous other evil chances of life. But handle Queequeg's monkey-rope
+                    heedfully as I would, sometimes he jerked it so, that I came very near sliding
+                    overboard. Nor could I possibly forget that, do what I would, I only had the
+                    management of one end of it.
+                </p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+    name: "AboutView",
+});
+</script>
