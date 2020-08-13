a471383bef05cf26be8e013e4d54274a5cf7a682
audricus19 <audricus2019@gmail.com>
2020-08-12 21:15:49 -0700

    added cashier pages(bitcoin, paypal, cc, bank)

---
+++ resources/js/app/molecules/cashier/BitcoinForm.vue
@@ -0,0 +1,42 @@
+<template>
+    <form class="form">
+        <label class="label label--large">AMOUNT TO DEPOSIT</label>
+        <div class="form__control">
+            <div class="form__control__icon form__control__icon--right">
+                <i class="icon icon--micro icon--usd icon--color--light-1"></i>
+            </div>
+            <input class="input input--large" placeholder="Minimum of $50" />
+        </div>
+        <div class="seperator"></div>
+        <div class="form__control m--b--6">
+            <label class="label label--large">BTC WALLET</label>
+            <input class="input input--large" />
+        </div>
+        <div class="form__control m--b--10">
+            <label class="label label--large">COUNTRY</label>
+            <div class="dropdown">
+                <div class="form__control">
+                    <input class="input input--large" type="text" value="USA" readonly="readonly" />
+                    <div class="form__control__icon--right">
+                        <i class="icon icon--micro icon--down"></i>
+                    </div>
+                </div>
+                <div class="dropdown__content">
+                    <div class="dropdown__content__item">
+                        USA
+                        <i class="icon icon--smaller icon--check icon--color--yellow-1"></i>
+                    </div>
+                    <div class="dropdown__content__item">England</div>
+                </div>
+            </div>
+        </div>
+        <button class="button button--large">SUBMIT</button>
+    </form>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+    name: "BitcoinForm",
+});
+</script>
---
+++ resources/js/app/molecules/cashier/CreditCardForm.vue
@@ -0,0 +1,51 @@
+<template>
+    <form class="form">
+        <label class="label label--large">AMOUNT TO DEPOSIT</label>
+        <div class="form__control">
+            <div class="form__control__icon form__control__icon--right">
+                <i class="icon icon--micro icon--usd icon--color--light-1"></i>
+            </div>
+            <input class="input input--large" placeholder="Minimum of $50" />
+        </div>
+        <div class="seperator"></div>
+        <div class="gap m--b--6">
+            <div class="gap__item">
+                <div class="form__control">
+                    <label class="label label--large">CARD NUMBER</label>
+                    <input class="input input--large" placeholder="1234 5678 9012 3456" />
+                </div>
+            </div>
+            <div class="gap__item">
+                <div class="form__control">
+                    <label class="label label--large">NAME ON CARD</label>
+                    <input class="input input--large" placeholder="Ex. John Doe" />
+                </div>
+            </div>
+        </div>
+        <div class="gap m--b--10">
+            <div class="gap__item">
+                <div class="form__control">
+                    <label class="label label--large">EXPIRATION DATE</label>
+                    <input class="input input--large" placeholder="__/__" />
+                </div>
+            </div>
+            <div class="gap__item">
+                <label class="label label--large">SECURITY CODE</label>
+                <div class="form__control">
+                    <div class="form__control__icon form__control__icon--right">
+                        <i class="icon icon--micro icon--wallet icon--color--light-1"></i>
+                    </div>
+                    <input class="input input--large" placeholder="CVV" />
+                </div>
+            </div>
+        </div>
+        <button type="submit" class="button button--large">SUBMIT</button>
+    </form>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+    name: "CreditCardForm",
+});
+</script>
