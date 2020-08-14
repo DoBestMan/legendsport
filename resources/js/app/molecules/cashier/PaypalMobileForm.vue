65c896b9017f8003851d50185c6d4571e997cce2
audricus19 <audricus2019@gmail.com>
2020-08-13 21:09:29 -0700

    added cashier paypal form for mobile

---
+++ resources/js/app/molecules/cashier/PaypalMobileForm.vue
@@ -0,0 +1,33 @@
+<template>
+    <section>
+        <div class="paging">
+            <div class="paging__item">
+                <i
+                    class="icon icon--left icon--large icon--color--light-1 m--r--4"
+                    @click="goToCashier"
+                ></i>
+                <div class="paging__item__title">Cashier</div>
+            </div>
+        </div>
+
+        <div class="container container--small">
+            <PaypalForm />
+        </div>
+    </section>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import PaypalForm from "./PaypalForm.vue";
+export default Vue.extend({
+    name: "PaypalMobileForm",
+
+    components: { PaypalForm },
+
+    methods: {
+        goToCashier(): void {
+            this.$router.push("/cashier");
+        },
+    },
+});
+</script>
