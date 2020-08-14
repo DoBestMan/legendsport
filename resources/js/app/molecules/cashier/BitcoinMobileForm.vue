903f41b16a8acf00f94be735c984fc6dca049b55
audricus19 <audricus2019@gmail.com>
2020-08-13 21:08:40 -0700

    added cashier bitcoin form for mobile

---
+++ resources/js/app/molecules/cashier/BitcoinMobileForm.vue
@@ -0,0 +1,33 @@
+<template>
+    <section>
+        <div class="paging">
+            <div class="paging__item">
+                <i
+                    class="icon icon--left icon--large icon--color--light-1 m--r--4"
+                    @click="goToCashier"
+                ></i>
+                <div class="paging__item__title">Cashier</div>
+            </div>
+        </div>
+
+        <div class="container container--small">
+            <BitcoinForm />
+        </div>
+    </section>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import BitcoinForm from "./BitcoinForm.vue";
+export default Vue.extend({
+    name: "BitcoinMobileForm",
+
+    components: { BitcoinForm },
+
+    methods: {
+        goToCashier(): void {
+            this.$router.push("/cashier");
+        },
+    },
+});
+</script>
