80c3c0b08a60eb433cd4fdb5dd56ce312ae111c3
Carnage <t.carnage@gmail.com>
2020-08-14 16:00:02 +0100

    Deploy worker

--- infrastructure/build/staging/deploy.sh
+++ infrastructure/build/staging/deploy.sh
@@ -14,6 +14,7 @@ echo "Deploying: $BACKEND_IMAGE and $FRONTEND_IMAGE"
 sed "s!BACKEND_IMAGE!${BACKEND_IMAGE}!g" ./infrastructure/kubernetes/staging/templated/php-fpm.yaml | sed "s!FRONTEND_IMAGE!${FRONTEND_IMAGE}!g" > /tmp/staging/php-fpm.yaml
 sed "s!BACKEND_IMAGE!${BACKEND_IMAGE}!g" ./infrastructure/kubernetes/staging/templated/scheduler.yaml > /tmp/staging/scheduler.yaml
 sed "s!BACKEND_IMAGE!${BACKEND_IMAGE}!g" ./infrastructure/kubernetes/staging/templated/websockets.yaml > /tmp/staging/websockets.yaml
+sed "s!BACKEND_IMAGE!${BACKEND_IMAGE}!g" ./infrastructure/kubernetes/staging/templated/worker.yaml > /tmp/staging/worker.yaml
 
 gcloud container clusters get-credentials --region "$CLOUDSDK_COMPUTE_REGION" "$CLOUDSDK_CONTAINER_CLUSTER"
 
