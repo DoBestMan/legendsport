74cce43ebb7e70c83aa1a422546f843a7e5988c1
audricus19 <audricus2019@gmail.com>
2020-08-14 09:42:10 -0700

    fixed sign in and sign up redirect issue

--- resources/js/app/molecules/auth/SignUpForm.vue
+++ resources/js/app/molecules/auth/SignUpForm.vue
@@ -1,63 +1,7 @@
 <template>
-    <!-- <form @submit.prevent="signUp">
-        <div class="form-group">
-            <label for="form-name">Name</label>
-            <FormInput
-                id="form-name"
-                inputClass="form-control form-control-lg"
-                type="text"
-                autocomplete="username"
-                :errors="errors.name"
-                v-model="name"
-                required
-            />
-        </div>
-        <div class="form-group">
-            <label for="form-email">E-mail</label>
-            <FormInput
-                id="form-email"
-                inputClass="form-control form-control-lg"
-                type="email"
-                autocomplete="email"
-                :errors="errors.email"
-                v-model="email"
-                required
-            />
-        </div>
-        <div class="form-group">
-            <label for="form-password">Password</label>
-            <FormInput
-                id="form-password"
-                inputClass="form-control form-control-lg"
-                type="password"
-                autocomplete="current-password"
-                :errors="errors.password"
-                v-model="password"
-                minlength="8"
-                required
-            />
-        </div>
-        <div class="form-group">
-            <label for="form-password-confirmation">Confirm Password</label>
-            <FormInput
-                id="form-password-confirmation"
-                inputClass="form-control form-control-lg"
-                type="password"
-                autocomplete="current-password"
-                :errors="errors.password_confirmation"
-                v-model="passwordConfirmation"
-                minlength="8"
-                required
-            />
-        </div>
-        <div class="form-group mt-5">
-            <button type="submit" class="btn btn-action btn-block btn-lg">Sign Up</button>
-        </div>
-    </form> -->
-
     <div class="layout">
         <div class="layout__navbar layout__navbar--transparent">
-            <div class="logo">
+            <div class="logo" @click="goToHome">
                 <div class="logo__icon">LS</div>
                 <div class="logo__text d--only--desktop">LEGEND SPORTS</div>
             </div>
@@ -69,26 +13,26 @@
                     <div class="form__control m--b--4">
                         <label class="label label--large">E-MAIL</label>
                         <FormInput
-                            id="form-name"
-                            inputClass="input input--large"
-                            placeHolder="Ex. john@google.com"
-                            type="text"
-                            autocomplete="username"
-                            :errors="errors.name"
-                            v-model="name"
-                            required
-                        />
-                    </div>
-                    <div class="form__control m--b--4">
-                        <label class="label label--large">Email</label>
-                        <FormInput
                             id="form-email"
                             inputClass="input input--large"
-                            placeHolder="Pick a email"
+                            placeHolder="Ex. john@google.com"
                             type="email"
                             autocomplete="email"
                             :errors="errors.email"
                             v-model="email"
+                            required
+                        />
+                    </div>
+                    <div class="form__control m--b--4">
+                        <label class="label label--large">USERNAME</label>
+                        <FormInput
+                            id="form-name"
+                            inputClass="input input--large"
+                            placeHolder="Pick a username"
+                            type="text"
+                            autocomplete="username"
+                            :errors="errors.name"
+                            v-model="name"
                             required
                         />
                     </div>
@@ -159,6 +103,10 @@ export default Vue.extend({
     },
 
     methods: {
+        goToHome(): void {
+            this.$router.push("/");
+        },
+
         async signUp() {
             try {
                 await this.$stock.state.api.signUp({
@@ -167,7 +115,8 @@ export default Vue.extend({
                     password: this.password,
                     password_confirmation: this.passwordConfirmation,
                 });
-                this.$emit("success");
+                this.$stock.dispatch("user/reload");
+                this.$router.push("/");
             } catch (e) {
                 this.$toast.error((e as AxiosError).response?.data.message);
                 this.errors = (e as AxiosError).response?.data.errors ?? {};
