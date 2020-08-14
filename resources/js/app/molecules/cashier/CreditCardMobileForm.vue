84999610877843074b7dc5d6e209045105027d5f
audricus19 <audricus2019@gmail.com>
2020-08-13 21:09:11 -0700

    added cashier creditcard form for mobile

---
+++ resources/js/app/molecules/cashier/CreditCardMobileForm.vue
@@ -0,0 +1,33 @@
+<template>
+    <section>
+        <div class="paging">
+            <div class="paging__item">
+                <i
+                    class="icon icon--left icon--large icon--color--light-1 m--r--4"
+                    @click="goToCashier"
+                ></i>
+                <div class="paging__item__title">Cashier</div>
+            </div>
+        </div>
+
+        <div class="container container--small">
+            <CreditCardForm />
+        </div>
+    </section>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import CreditCardForm from "./CreditCardForm.vue";
+export default Vue.extend({
+    name: "CreditCardMobileForm",
+
+    components: { CreditCardForm },
+
+    methods: {
+        goToCashier(): void {
+            this.$router.push("/cashier");
+        },
+    },
+});
+</script>
