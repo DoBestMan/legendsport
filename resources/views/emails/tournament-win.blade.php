cb6032f4fcbcd9c55d87f7b60557b8c62b96af0c
carnage <carnage@users.noreply.github.com>
2020-10-22 15:01:34 +0100

    Update tournament-win.blade.php

--- resources/views/emails/tournament-win.blade.php
+++ resources/views/emails/tournament-win.blade.php
@@ -3,7 +3,7 @@
     <h4 style="color: #fff; font: normal 18px Arial, Helvetica, sans-serif; line-height: normal; margin: 0 0 9px 0; text-align: center;">Congratulations {{ $name }}</h4>
     <p style="color: #fff; font: normal 16px Arial, Helvetica, sans-serif; line-height: 22px; margin: 0 0 49px 0; text-align: center;">
 
-        You have placed in the money in the ${{ $tournamentName }} tournament your balance has been credited with your {{ $place }}
+        You have placed in the money in the ${{ $tournamentName }} tournament your balance has been credited with your {{ $rank }}
         winnings of ${{ number_format($amount, 2) }}
 
         Legend Sports
