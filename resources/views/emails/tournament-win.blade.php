c151158e4e18d8a59a9f96decd0d3e6e406751df
Carnage <t.carnage@gmail.com>
2020-10-23 14:40:38 +0100

    Change winners email text

--- resources/views/emails/tournament-win.blade.php
+++ resources/views/emails/tournament-win.blade.php
@@ -2,10 +2,7 @@
 @section('email-content')
     <h4 style="color: #fff; font: normal 18px Arial, Helvetica, sans-serif; line-height: normal; margin: 0 0 9px 0; text-align: center;">Congratulations {{ $name }}</h4>
     <p style="color: #fff; font: normal 16px Arial, Helvetica, sans-serif; line-height: 22px; margin: 0 0 49px 0; text-align: center;">
-
-        You have placed in the money in the {{ $tournamentName }} tournament your balance has been credited with your {{ str_ordinal($rank) }} place
-        winnings of ${{ number_format($amount, 2) }}
-
-        Legend Sports
+        You finished in {{ str_ordinal($rank) }} place in {{ $tournamentName }} tournament and your real money account has been credited with
+        ${{ number_format($amount / 100, 2) }}
     </p>
 @endsection
